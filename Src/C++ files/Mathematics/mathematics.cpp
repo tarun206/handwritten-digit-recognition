c07708be52517c1d95d8687806b3d87f046694d8
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-01-25 15:42:25 +0530

    Added a function

--- Src/C++ files/Mathematics/mathematics.cpp
+++ Src/C++ files/Mathematics/mathematics.cpp
@@ -11,4 +11,37 @@ public:
         double result = sigmoid(point) - Math.pow(sigmoid(point),2);
         return result;
     }
+
+    double probability(int P_Array[], long length_of_array, int X)
+    {
+        long number_of_occurences = 0;
+        for (int i = 0; i < length_of_array; i++)
+        {
+            if (P_Array[i] == X)
+                number_of_occurences++;
+        }
+        double P_X = number_of_occurences/length_of_array;
+
+        return P_X;
+    }
+
+    double compute_log10(double X)
+    {
+        double log10_X = 1, intermediate_result = 1;
+        long iterations = 0;
+
+        while (1)
+        {
+            /* Explanation of the below statements
+            *
+            */
+            for (int i = 0; i < iterations; Y++)
+            {
+                intermediate_result = 10 * 10;
+                if (log10_X = X)
+                    break;
+            }
+        }
+        return log_X;
+    }
 }
