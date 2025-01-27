e2ed653cec4d598191718cb165fe34379317a706
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-01-27 13:56:52 +0530

    Added functions to calculate pixel spikes and distance from one pixel to another or radius of a curve

--- src/C++ files/Image processing/digital_image_processing.cpp
+++ src/C++ files/Image processing/digital_image_processing.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <decision_tree.hpp>
 
 class image_processing{
 private:
     int image[28][28];
+    vector<long> spike_dataset, radius_dataset;
     char character_buffer[2];
 public:
     void load_image()
@@ -70,6 +72,73 @@ public:
         for (int i = 0; i < 28; i++)
             for (int j = 0; j < 28; j++)
                 image[i][j] = 256 - image[i][j];
+    }
+
+    void count_spikes_and_push_to_dataset()
+    {
+        int large_pixel_difference = 0, current_pixel = 0, next_pixel = 0, pixel_difference = 0;
+        long spike_count = 0;
+
+        /** Explanation of below statements:
+        *   the below loops go over the image and subtract the pixel values.
+        *   if the pixel value difference is large enough and is consistent and above a threshold, it is considered to be one of the numbers.
+        *   For example,
+        *   Number 1 can be determined by 3 spikes in pixel values.
+        *   2 can be determined by 6 spikes in pixel values.
+        *   3 can be determined by 6 spikes in pixel values.
+        *   4 can be determined by 4 spikes in pixel values.
+        */
+
+        for (int i = 0; i < 28; i++)
+            for (int j = 0; j < 28; j++)
+            {
+                if (j == 28-1)
+                    break;
+                else{
+
+                    current_pixel = image[i][j];
+                    next_pixel = image[i][j+1];
+                    pixel_difference = current_pixel - next_pixel;
+
+                    if (pixel_difference > 100)
+                        spike_count++;
+                }
+            }
+        dataset.push_back(spike_count);
+        return dataset;
+    }
+
+    void calculate_radius_and_push_to_dataset()
+    {
+        /** Explanation of the below statements:
+        *   Calculates the distance between the first spike in pixel values and the second spike in pixel values.
+        */
+
+        int current_pixel_value = 0, next_pixel_value = 0, pixel_difference = 0;
+        long first_spike_pixel = 0, second_spike_pixel = 0, radius = 0;
+
+        for (int i = 0; i < 28; i++)
+        {
+            for (int j = 0; j < 28; j++)
+            {
+                if (j+1 == 28-1)
+                    continue;
+                else{
+                    current_pixel_value = image[i][j];
+                    next_pixel_value = image[i][j+1];
+                    pixel_diffrence = current_pixel_value - next_pixel_value;
+                    if (pixel_diffrence > 100)
+                    {
+                        if (first_spike_pixel == 0)
+                            first_spike_pixel = (i+j+1);
+                        else if (first_spike_pixel > 0 && second_spike_pixel == 0)
+                            second_spike_pixel = (i+j+1);
+                    }
+                }
+            }
+            radius = first_spike_pixel - second_spike_pixel;
+            radius_dataset.push_back(radius);
+        }
     }
 };
 
--- src/C++ files/Machine learning/decision_tree.cpp
+++ src/C++ files/Machine learning/decision_tree.cpp
@@ -1,4 +1,50 @@
+#include <iostream>
+#include <string>
+
+struct node{
+    string label;
+    node *left; // Less than the value being compared.
+    node *right; // Greater than the value being compared.
+    node * middle; // Equal to the value being compared.
+    node *previous; // Pointer to the previous node.
+    vector <node*> edges;
+};
+
 class decision_tree{
 public:
+    node* instantiate_root (string label)
+    {
+        node *root = new node();
+        root -> label = label;
+        root -> previous = NULL;
+        root -> left = NULL;
+        root -> right = NULL;
+        root -> middle = NULL;
+        return root;
+    }
 
-}
+    node* insert_node (node* current_node, string label, long number_of_spikes, long radius_of_curve)
+    {
+        node *new_node = new node();
+        new_node -> label = label;
+        new_node -> left = NULL;
+        new_node -> right = NULL;
+        new_node -> middle = NULL;
+        new_node -> previous = current_node;
+        return new_node;
+    }
+
+    node* edit_node (node *current_node, string new_label, long number_of_spikes, long radius_of_curve, node *previous_node)
+    {
+        current_node -> label = new_label;
+        current_node -> left = ;
+        current_node -> middle = ;
+        current_node -> right = ;
+        current_node -> previous = previous_node;
+    }
+
+    node* delete_tree (node *root)
+    {
+        return NULL;
+    }
+};
